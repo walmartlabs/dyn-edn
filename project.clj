855a775959cf1bec531a303a323e6f05f7b260fb
heykieran <heykieran@gmail.com>
2020-05-01 09:00:32 -0400

    Updates the README file and added a deps.edn file with updated version of Clojure.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.org/walmartlabs/dyn-edn"
   :license {:name "Apache Software License"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :plugins [[lein-codox "0.10.3"]]
   :codox {:source-uri "https://github.com/walmartlabs/dyn-edn/blob/master/{filepath}#L{line}"
           :metadata {:doc/format :markdown}})
